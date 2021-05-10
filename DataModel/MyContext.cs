993ac9382e53f8d16e37f1340aa36d56b985b77b
Harry Vu <harryvu@outlook.com>
2021-05-10 03:20:40 -0700

    First commit. POCO classes, MyContext, connection string.

---
+++ DataModel/MyContext.cs
@@ -0,0 +1,21 @@
+﻿using MyDomain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+	public class MyContext : DbContext
+	{
+		public MyContext()
+			:base("name=MyDb")
+		{
+
+		}
+		public DbSet<Employee> Employees { get; set; }
+		public DbSet<Department> Departments { get; set; }
+	}
+}
---
+++ MyDomain/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDomain
+{
+	public class Employee
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		public Department Department { get; set; }
+		public int DepartmentId { get; set; }
+	}
+}
